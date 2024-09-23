ae62385eb2337b80e06be6c06452dd1db048a70f
lucas <lucasccesar05@gmail.com>
2024-09-23 20:44:58 -0300

    continued

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
-let sliderButtons = document.getElementsByClassName('sliderPass')
-let slider = document.querySelector('#sliderDiv')
-let sliderInner = document.querySelector('#sliderDiv div')
-let sliderPosition = 0
+let sliderButtons = document.querySelectorAll('.sliderPass');
+let diaButtons = document.querySelectorAll('.diaButton');
+let cronogramas = document.getElementById('cronogramas')
+let sobreCards = document.querySelectorAll('.sobreCards')
 
 Object.values(sliderButtons).forEach((element) => {
-    element.addEventListener('click', ()=>{
-        if(element.id == 'sliderForward' && sliderPosition/slider.clientWidth * -1 != sliderInner.childElementCount - 1){
-            sliderInner.style.transform = `translateX(${sliderPosition - slider.clientWidth}px)`
-            sliderPosition -= slider.clientWidth
-            sliderButtons[0].classList.remove('sliderPassHidden')
-            if(sliderPosition/slider.clientWidth * -1 != sliderInner.childElementCount - 2){
-                sliderButtons[1].classList.add('sliderPassHidden')
+    element.addEventListener('click', () => {
+        let slider = element.parentElement.children[1]; // slider
+        let sliderInner = slider.firstElementChild; // sliderInner
+        element.classList.contains('sliderForward'); // element.id == 'sliderForward'
+        let currentSliderButtons = [element.parentElement.firstElementChild, element.parentElement.lastElementChild];
+
+        console.log(sliderInner.dataset.position);
+
+        if (element.classList.contains('sliderForward') == true && (sliderInner.dataset.position / slider.clientWidth) * -1 != sliderInner.childElementCount - 1) {
+            sliderInner.style.transform = `translateX(${sliderInner.dataset.position - slider.clientWidth}px)`;
+            sliderInner.dataset.position = `${sliderInner.dataset.position - slider.clientWidth}`;
+            currentSliderButtons[0].classList.remove('sliderPassHidden');
+            if ((sliderInner.dataset.position / slider.clientWidth) * -1 != sliderInner.childElementCount - 2) {
+                currentSliderButtons[1].classList.add('sliderPassHidden');
             }
-        } else if(element.id == 'sliderBackward' && sliderPosition/slider.clientWidth * -1 != 0){
-            sliderInner.style.transform = `translateX(${sliderPosition + slider.clientWidth}px)`
-            sliderPosition += slider.clientWidth
-            sliderButtons[1].classList.remove('sliderPassHidden')
-            if(sliderPosition/slider.clientWidth * -1 == 0){
-                sliderButtons[0].classList.add('sliderPassHidden')
+        } else if (element.classList.contains('sliderBackward') == true && (sliderInner.dataset.position / slider.clientWidth) * -1 != 0) {
+            sliderInner.style.transform = `translateX(${parseInt(sliderInner.dataset.position) + parseInt(slider.clientWidth)}px)`;
+            sliderInner.dataset.position = `${parseInt(sliderInner.dataset.position) + parseInt(slider.clientWidth)}`;
+            currentSliderButtons[1].classList.remove('sliderPassHidden');
+            if ((sliderInner.dataset.position / slider.clientWidth) * -1 == 0) {
+                currentSliderButtons[0].classList.add('sliderPassHidden');
             }
         }
+    });
+});
+
+diaButtons.forEach((element, index) => {
+    element.addEventListener('click', ()=>{func(element, index)});
+});
+
+function func(e, i){
+    if(e.classList.contains('diaIndisponivel') != true){
+        cronogramas.style.transform = `translateX(${cronogramas.clientWidth * i * -1}px)`
+        document.querySelector('.diaSelecionado').classList.remove('diaSelecionado')
+        e.classList.add('diaSelecionado')
+    }
+}
+
+sobreCards.forEach(e => {
+    e.addEventListener('mouseenter', ()=>{
+        e.lastElementChild.style.height = `${e.lastElementChild.firstElementChild.clientHeight}px`
+        e.firstElementChild.style.width = `5vw`
+        console.log(e.lastElementChild.firstElementChild.clientHeight)
+    })
+    e.addEventListener('mouseleave', ()=>{
+        e.lastElementChild.style.height = `0px`
+        e.firstElementChild.style.width = `8vw`
+        console.log(e.lastElementChild.firstElementChild.clientHeight)
     })
 });
